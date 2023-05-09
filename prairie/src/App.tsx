6381b6c4a34fd31e65c06352cd62e7e4b18e0f5c
margaretsoler <margaretamsoler@gmail.com>
2023-05-09 12:50:41 +0200

    test normal video player
    >different disposition next time

--- prairie/src/App.tsx
+++ prairie/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import VideoFeed from './VideoFeed'
+import ReactPlayer from 'react-player'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,18 +19,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>MERCI ELO </h1>
+      <h1>village prairie</h1>
       <div className="app">
-        Hello World!
-        <VideoFeed src="http://localhost:8083/stream/pattern/channel/0/hls/live/index.m3u8" />
+        live
+        <VideoFeed src="http://localhost:8083/stream/pattern/channel/0/hls/live/index.m3u8"  />
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <ReactPlayer url={'src/assets/IMG_9910.mp4'} controls={true} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
