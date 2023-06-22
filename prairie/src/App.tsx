2904ba24542162dd3e6667bb2d8c88548fbcff7c
margaretsoler <margaretamsoler@gmail.com>
2023-06-23 00:46:23 +0200

    night mode !!

--- prairie/src/App.tsx
+++ prairie/src/App.tsx
@@ -1,31 +1,20 @@
-import './App.css'
-import HelloWorld from './Carotte';
-import VideoFeed from './VideoFeed'
-import { Box } from "@mui/system";
 import DelayedTime from './DelayedTime';
-
-
-
-
+import './App.css';
+import HelloWorld from './Carotte';
+import VideoFeed from './VideoFeed';
+import { Box } from "@mui/system";
 
 function App() {
   return (
-    <Box sx={{ position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh",}}>
-      <HelloWorld></HelloWorld>
-      <VideoFeed src="http://localhost:8083/stream/pattern/channel/0/hls/live/index.m3u8"  />
+    <Box sx={{ position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh" }}>
       <DelayedTime />
-        
-
-      <div>
-    
-  </div>
-      
+      <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}>
+        <HelloWorld />
+        <VideoFeed src="http://localhost:8083/stream/pattern/channel/0/hls/live/index.m3u8" />
+      </Box>
     </Box>
-  )
+  );
 }
 
+export default App;
 
-
-
-
-export default App
--- prairie/src/DelayedTime.tsx
+++ prairie/src/DelayedTime.tsx
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 const DelayedTime: React.FC = () => {
   const [time, setTime] = useState<string>('');
+  const [isDaytime, setIsDaytime] = useState(false);
 
   useEffect(() => {
     const delay = 10 * 60 * 1000; // 10 minutes in milliseconds
 
     const updateDelayedTime = () => {
-      const delayedTime = new Date(Date.now() + delay);
+      const currentDelayedTime = new Date();
+      const delayedTime = new Date(currentDelayedTime.getTime() + delay);
       const formattedTime = delayedTime.toLocaleTimeString();
       setTime(formattedTime);
+
+      const currentHour = delayedTime.getHours();
+      setIsDaytime(currentHour >= 6 && currentHour < 18);
     };
+
+    // Call the function initially
+    updateDelayedTime();
 
     const intervalId = setInterval(updateDelayedTime, 1000); // Update every second
 
@@ -19,7 +27,13 @@ const DelayedTime: React.FC = () => {
     };
   }, []);
 
-  return <div  className="time-font">{time}</div>;
+  const backgroundColor = isDaytime ? 'white' : 'black';
+
+  return (
+    <div style={{ backgroundColor, minHeight: '100vh' }}>
+      <div className="time-font">{time}</div>
+    </div>
+  );
 };
 
 export default DelayedTime;
