7bb90f1c8f95b4ab9cc0f541c2de2b2235a4ed39
valentinbreiz <valentinbreiz@gmail.com>
2023-06-29 21:21:12 +0200

    Add tomato when play pause

--- prairie/src/VideoFeed.tsx
+++ prairie/src/VideoFeed.tsx
@@ -1,10 +1,13 @@
 import { useRef, useEffect, useState } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
+import tomateImg from "./tomate.png";
+import tomateImg2 from "./tomate2.png";
 
 const VideoFeed: React.FC<VideoFeedProps> = ({ src }) => {
   const videoRef = useRef(null);
   const [player, setPlayer] = useState<ReturnType<typeof videojs>>();
+  const [isPlaying, setIsPlaying] = useState(false); // Ajout du state pour vérifier si la vidéo est en cours de lecture
 
   useEffect(() => {
     // make sure Video.js player is only initialized once
@@ -28,9 +31,22 @@ const VideoFeed: React.FC<VideoFeedProps> = ({ src }) => {
     };
   }, [player]);
 
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const handlePause = () => {
+    setIsPlaying(false);
+  };
+
   return (
     <div className="video-container">
-      <video className="video-js" ref={videoRef} controls>
+      {isPlaying ? (
+        <img src={tomateImg2} alt="Tomate" className="tomate-image" />
+      ) : (
+        <img src={tomateImg} alt="Tomate" className="tomate-image" />
+      )}
+      <video onPlay={handlePlay} onPause={handlePause} className="video-js" ref={videoRef} controls>
         <source src={src} type="application/x-mpegURL" />
       </video>
     </div>
@@ -44,4 +60,3 @@ interface VideoFeedProps {
 export default VideoFeed;
 
 
-	
