1274b605d06dfa5521c37fb85c8ac2a815614b8c
gencive5 <vic.segen@gmail.com>
2024-07-29 17:35:14 +0200

    autoreloading (vite) works now!

--- prairie/vite.config.ts
+++ prairie/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: 'localhost', // or '0.0.0.0' if you need to access from other devices
     port: 3000,
+    watch: {
+      usePolling: true, // Use polling to watch for file changes
+    },
+    hmr: {
+      protocol: 'ws', // Ensure WebSocket protocol is used for HMR
+      host: 'localhost',
+    },
   },
 })
